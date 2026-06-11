5a84663c3145724928a22c56b16b9616947ebf5b
Nezzal <nezzal.abdelmalek@gmail.com>
2026-06-11 21:31:33 +0100

    chore: bump version to v1.3.0

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '1.2.0';
+export const APP_VERSION = '1.3.0';
 export const APP_NAME = 'METHODO-CLINIQUE Édu';
 export const APP_VERSION_LABEL = `${APP_NAME} v${APP_VERSION} - Production Ready`;
