31c16027381413b318b2d9e45bccc259a9311638
Nezzal <nezzal.abdelmalek@gmail.com>
2026-07-28 11:06:07 +0100

    feat: publication officielle de la version v1.8.6 sur Vercel

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,4 @@ export const APP_VERSION = '1.8.6';
 export const APP_NAME = 'METHODO-CLINIQUE Édu';
 export const APP_VERSION_LABEL = `${APP_NAME} v${APP_VERSION} - Production Ready`;
 export const COMPANY_NIF = '15007180115910202380';
+// Build release trigger v1.8.6
