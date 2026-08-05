24c4e20a0f0881bfe01a483c4169f90150a82786
Nezzal <nezzal.abdelmalek@gmail.com>
2026-08-05 17:10:30 +0100

    fix(cache): add auto-reload SW unregister script to force Safari/Chrome to purge old v2.0.7 cache and reload v2.0.6

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,21 @@
-self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
-      .then(() => self.clients.claim())
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => caches.delete(key)));
+    }).then(() => {
+      return self.registration.unregister();
+    }).then(() => {
+      return self.clients.matchAll({ type: 'window' });
+    }).then((clients) => {
+      clients.forEach((client) => {
+        if (client.url && 'navigate' in client) {
+          client.navigate(client.url);
+        }
+      });
+    })
   );
 });
