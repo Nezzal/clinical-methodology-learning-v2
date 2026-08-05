8d38e32f2bf5f336b9dfd1d5f87eab686b18fbff
Nezzal <nezzal.abdelmalek@gmail.com>
2026-08-05 16:43:41 +0100

    fix(cache): add SW unregister script to force browsers to purge v2.0.7 cache and load v2.0.6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,18 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,400..800;1,400..800&family=Inter:wght@300;400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet" />
+        <script dangerouslySetInnerHTML={{ __html: `
+          if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.getRegistrations().then(function(registrations) {
+              for (let registration of registrations) { registration.unregister(); }
+            });
+          }
+          if ('caches' in window) {
+            caches.keys().then(function(names) {
+              for (let name of names) { caches.delete(name); }
+            });
+          }
+        ` }} />
       </head>
       <body suppressHydrationWarning>
         <AuthProvider>
